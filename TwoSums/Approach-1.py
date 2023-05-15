3951da0bd7c6a1a37ba212bb4c66b8a7bce80af2
Jinesh Mehta <mehta.jin@northeastern.edu>
2023-05-15 14:20:00 -0400

    approach 1 template

---
+++ TwoSums/Approach-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pass
+
+
+def main():
+    s = Solution()
+    print(s.twoSum([2, 7, 11, 15], 9))
+    print(s.twoSum([3, 2, 4], 6))
+    print(s.twoSum([3, 3], 6))
+
+
+if __name__ == "__main__":
+    main()
