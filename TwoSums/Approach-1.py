6ef2966aacd5adde4f5b1d76c79c5bc117c5ef4d
Jinesh Mehta <mehta.jin@northeastern.edu>
2023-05-15 15:13:30 -0400

    completed two sums problem

--- TwoSums/Approach-1.py
+++ TwoSums/Approach-1.py
@@ -1,6 +1,12 @@
+from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+        for index_x in range(len(nums)):
+            for index_y in range(index_x + 1, len(nums)):
+                if nums[index_x] + nums[index_y] == target:
+                    return [index_x, index_y]
+        # Time Complexity : O(N^2)
+        # Space Complexity : O(1)
 
 
 def main():
